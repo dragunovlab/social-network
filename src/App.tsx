2a5c765df05f3ea7a8417935d9b4eaf13f45aa8e
dragunovlab <dragunov.lab@gmail.com>
2025-05-29 11:49:35 +0300

    chore: initial project setup with basic App component

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div>
+    <div>one div</div>
+    <div className="App">
+    <ul>
+      <li>html</li>
+      <li>css</li>
+      <li>javascript</li>
+      <li>react</li>
+    </ul>
+    </div>
+    </div>
+  )
+}
+
+export default App
+
+
+
