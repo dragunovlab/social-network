3c30b1eb3ec17372751ce01223a6a25dc456526d
dragunovlab <dragunov.lab@gmail.com>
2025-06-16 20:07:10 +0600

    feat: implement React Router for application navigation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,19 +4,19 @@ const Navbar = () => {
     return (
          <nav className={s.nav}>
     <div className={`${s.item} ${s.active}`}>
-      <a>Profile</a>
+      <a href='/profile'>Profile</a>
     </div>
     <div className={s.item}>
-      <a>Messages</a>
+      <a href='/dialogs'>Messages</a>
     </div>
     <div className={s.item}>
-      <a>News</a>
+      <a href='/News'>News</a>
     </div>
     <div className={s.item}>
-      <a>Music</a>
+      <a href='/Music'>Music</a>
     </div>
     <div className={s.item}>
-      <a>Settings</a>
+      <a href='/Settings'>Settings</a>
     </div>
   </nav>
     )
