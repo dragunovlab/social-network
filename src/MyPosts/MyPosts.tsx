34c6580674be2d0edeb7a74bf6ce35a1173d7ce1
dragunovlab <dragunov.lab@gmail.com>
2025-06-16 16:43:33 +0600

    feat: implement props system for Post component

--- src/MyPosts/MyPosts.tsx
+++ src/MyPosts/MyPosts.tsx
@@ -11,9 +11,9 @@ const MyPosts = () => {
                 <button>Remove post</button>
             </div>
             <div className={s.posts}>
-                <Post />
-                <Post />
-                <Post />
+                <Post message="Hi, how are you?" likesCount={15} />
+                <Post message="It's my first post" likesCount={20} />
+                <Post message="Blabla" likesCount={10} />
             </div>
         </div>
     )
--- src/MyPosts/Post/Post.tsx
+++ src/MyPosts/Post/Post.tsx
@@ -1,12 +1,12 @@
 import s from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src="https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png" alt="avatar" />
-            post 1
+            {props.message}
             <div>
-                <span>like</span>
+                <span>like {props.likesCount}</span>
             </div>
 
         </div>
