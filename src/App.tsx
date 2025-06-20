9ba6a2c4db7e6c1de57f8cd0c004edfd422d5c68
dragunovlab <dragunov.lab@gmail.com>
2025-06-20 13:19:37 +0600

    feat: implement NavLink with active state styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Dialogs from './components/Dialogs/Dialogs'
 import Profile from './components/Profile/Profile'
 import News from './components/News/News'
 import Music from './components/Music/Music'
-import Settings from './components/Settings/Setting'
+import Settings from './components/Settings/Settings'
 
 
 const App = () => {
@@ -21,9 +21,9 @@ const App = () => {
           <Routes>
             <Route path='/profile' element={<Profile />} />
             <Route path='/dialogs' element={<Dialogs />} />
-            <Route path='/News' element={<News />} />
-            <Route path='/Music' element={<Music />} />
-            <Route path='/Settings' element={<Settings />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/settings' element={<Settings />} />
           </Routes>
         </div>
       </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,28 @@
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
- 
+
 const Navbar = () => {
-    return (
-         <nav className={s.nav}>
-    <div className={`${s.item} ${s.active}`}>
-      <a href='/profile'>Profile</a>
-    </div>
-    <div className={s.item}>
-      <a href='/dialogs'>Messages</a>
-    </div>
-    <div className={s.item}>
-      <a href='/News'>News</a>
-    </div>
-    <div className={s.item}>
-      <a href='/Music'>Music</a>
-    </div>
-    <div className={s.item}>
-      <a href='/Settings'>Settings</a>
-    </div>
-  </nav>
-    )
+
+
+  return (
+    <nav className={s.nav}>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink to='/profile' className={navData => navData.isActive ? s.active : s.item}>Profile</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='/dialogs' className={navData => navData.isActive ? s.active : s.item}>Messages</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='/news' className={navData => navData.isActive ? s.active : s.item}>News</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='/music' className={navData => navData.isActive ? s.active : s.item}>Music</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='/settings' className={navData => navData.isActive ? s.active : s.item}>Settings</NavLink>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar;
